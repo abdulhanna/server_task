13ea33628ca840f233ff82083097638d1fb4330c
abdul hannan <abdul.hannan@plaxonic.com>
2024-02-26 00:11:48 +0530

    connection

--- src/helper/init.js
+++ src/helper/init.js
@@ -6,10 +6,11 @@ import mongoose from 'mongoose';
   
 //   let user = abdulhannan
   // let cluster = 'mongodb+srv://abdulhannan:XgMYoOUsgPNcEzOl@cluster0.mvhte7n.mongodb.net/'
-  let db = "mongodb+srv://abdulhannan:JVc0R1F4lkDMSyeb@cluster0.chphgsc.mongodb.net/?retryWrites=true&w=majority"
+  let db = "mongodb+srv://abdulhannan:JVc0R1F4lkDMSyeb@cluster0.chphgsc.mongodb.net/task?retryWrites=true&w=majority"
 
     try {
         const mongoUri = 'mongodb://localhost:27017/task_creator';
+
         await mongoose.connect(db, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
